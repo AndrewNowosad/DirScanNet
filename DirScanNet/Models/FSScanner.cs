84f2deb5e7c56a068aca1ef34f142e26a5183c32
Андрей <nowosad@inbox.ru>
2017-04-17 14:36:23 +0300

    Добавлен класс для удобного сканирования папок и дисков

---
+++ DirScanNet/Models/FSScanner.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DirScanNet.Models
+{
+    class FSScanner
+    {
+        public Folder GetFolder(string path) => new Folder(path);
+
+        public async Task<Folder> GetFolderAsync(string path)
+        {
+            return await Task.Run(() => GetFolder(path));
+        }
+
+        public IReadOnlyList<Folder> GetRootFolders()
+        {
+            return DriveInfo.GetDrives()
+                            .Select(d => GetFolder(d.RootDirectory.FullName))
+                            .ToList();
+        }
+
+        public async Task<IReadOnlyList<Folder>> GetRootFoldersAsync()
+        {
+            return await Task.Run(() => GetRootFolders());
+        }
+    }
+}
