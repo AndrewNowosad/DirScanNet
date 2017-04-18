f1570a56aeade0c63b449ea9f7d136862df76978
Андрей <nowosad@inbox.ru>
2017-04-18 14:35:35 +0300

    Добавлены стандартные VM-классы

---
+++ DirScanNet/ViewModels/DelegateCommand.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace DirScanNet.ViewModels
+{
+    class DelegateCommand : ICommand
+    {
+        protected readonly Predicate<object> _canExecute;
+        protected readonly Action<object> _execute;
+
+        public event EventHandler CanExecuteChanged;
+
+        public DelegateCommand(Action<object> execute, Predicate<object> canExecute = null)
+        {
+            _execute = execute ?? throw new NullReferenceException();
+            _canExecute = canExecute;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return _canExecute?.Invoke(parameter) ?? true;
+        }
+
+        public void Execute(object parameter)
+        {
+            _execute(parameter);
+        }
+
+        public void RaiseCanExecuteChanged()
+        {
+            CanExecuteChanged?.Invoke(this, EventArgs.Empty);
+        }
+    }
+}
