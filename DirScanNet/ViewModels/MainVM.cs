69bf5fa449d6d4942115a781183e229d47e0c206
Андрей <nowosad@inbox.ru>
2017-04-19 18:38:38 +0300

    Реализовал каркас представления и VM

--- DirScanNet/ViewModels/MainVM.cs
+++ DirScanNet/ViewModels/MainVM.cs
@@ -1,6 +1,49 @@
-﻿namespace DirScanNet.ViewModels
+﻿using DirScanNet.Models;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace DirScanNet.ViewModels
 {
     class MainVM : WindowVM
     {
+        Folder currentFolder;
+        public Folder CurrentFolder
+        {
+            get
+            {
+                return currentFolder;
+            }
+            set
+            {
+                Set(ref currentFolder, value);
+                NotifyPropertyChanged(nameof(Items));
+            }
+        }
+
+        public IEnumerable<FSItem> Items
+        {
+            get
+            {
+                return CurrentFolder?.ChildElements.OrderByDescending(item => item.Weight);
+            }
+        }
+
+        DelegateCommand scanFolder;
+        public ICommand ScanFolder
+        {
+            get
+            {
+                if (scanFolder == null)
+                    scanFolder = new DelegateCommand(async o => await ScanAsync((string)o));
+                return scanFolder;
+            }
+        }
+
+        protected async Task ScanAsync(string path)
+        {
+            CurrentFolder = await new FSScanner().GetFolderAsync(path);
+        }
     }
 }
